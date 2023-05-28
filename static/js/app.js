318ef4d1897d9a6207e6e3e51159c135baf6fe29
Jayplect <evansja.femi@gmail.com>
2023-05-28 02:00:21 -0400

    added dynamic plot for bargraph

--- static/js/app.js
+++ static/js/app.js
@@ -1,55 +1,106 @@
+// --------------------------BELLY BUTON DASHBOARD BUILD-------------------------//
+//////////////////////////////////////////////////////////////////////////////////
+
 // Use the D3 library to read in samples.json from the URL
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
-// Fetch the JSON data and console log it
+// Fetch the JSON data and console log it specified variables to peruse
 d3.json(url).then(function(data) {
-    console.log(data);
-  });
-// Display the default plot
-// let sampleValues = d3.select
-// let select = d3.select("#selDataset")
-
-// 
-// let option = select.append("option")
-
-// change text to otu_ids
-// let text = option.text("text")
-
-// On change to the DOM, call getData()
-// d3.selectAll("#selDataset").on("change", getData);
-// Function called by DOM changes
-
-d3.json(url).then(function(data){
     console.log(data)
+    console.log(data.names)
+    console.log(data.samples);
+  });
+//----------------Initialize default display-----------------//
+// Initialize and Display the default plot
+function init(){
+    // Create a drop-down menu for each id
+    let dropdown = d3.select("#selDataset");
+    // Fetch data
+    d3.json(url).then(function(data) {
+    // Append names to the dropdown menu using the forEach function
+        let nameIds = data.names
+        nameIds.forEach(function(nameId) {
+        dropdown.append("option").property("value", nameId).text(nameId)
+        });
+        // Select the first array of data for plotting
+        let sample1 = nameIds[0]
+        console.log(sample1)
+        updateBargraph(sample1)
+    })};
+init()
+//-------------------Function called by DOM changes-------------------//
+// On change to the DOM, call getData()
+// d3.selectAll("#selDataset").on("change", updatePlotly);
 
-    // console.log(dataDemoVal)
-    let trace1 = {
-                x: (data.samples[0].sample_values).slice(0,10).reverse(),
-                y: (data.samples[0].otu_ids).map(object => `OTU_${object}`).slice(0,10).reverse(),
-                name: "Demo",
-                type: "bar",
-                orientation: "h"
-                }
-    let traceData =[trace1]
+// // Function called by DOM changes
+// function updatePlotly() {
+//------------Bar Graph-----------------//
+function updateBargraph(selectName) {
+    // Fetch the JSON data
+    d3.json(url).then(function(data){
     
-    // Apply a title to the layout
-    let layout = {
-        title: " Top 10 OTUs ",
-        // margin: {
-        //     l: 100,
-        //     r: 100,
-        //     t: 100,
-        //     b: 100
-        // }
-        // height: 600,
-        // width: 800
-    };
-  
-    Plotly.newPlot("bar", traceData, layout);
-});
+    // Filter the id of the selected dropdown option in dataset
+    let sampleId = data.samples.filter(select => select.id === selectName)[0]
+    console.log(sampleId)
+    
+        let trace1 = {
+            x: (sampleId.sample_values).slice(0,10).reverse(),
+            y: (sampleId.otu_ids).map(object => `OTU_${object}`).slice(0,10).reverse(),
+            name: "Top 10 OTUs",
+            type: "bar",
+            text: (sampleId.otu_labels).slice(0,10).reverse(),
+            orientation: "h",
+            marker: {
+                color: '#C8A2C8'},
+            line: {
+                    width: 2.5
+                }
+            
+        };
+        let traceData =[trace1];
+
+        // Apply a title to the layout
+        let layout = {
+            title: " Top 10 OTUs ",
+            margin: {
+                l: 100,
+                r: 100,
+                t: 100,
+                b: 100
+            },
+            font: {size: 15}
+            // height: 600,
+            // width: 800
+        };
+
+        Plotly.newPlot("bar", traceData, layout);
+    })};
+    // Create a custom function to return players who made the team
+//     function Id(selectId) {return selectId.sample}
+//     // Call the custom function with filter()
+//     data.filter(Id)
+//     // Use D3 to select the dropdown menu
+//     let userselectedId = data.names
+    
+//     let dropdownMenu = d3.select("#selDataset").append("option").property("value", userselectedId).text();
+
+//     if (userselectedId == )
+
+//     // Assign the value of the dropdown menu option to a variable
+//     let dataset = dropdownMenu.property("value");
+// });
+
+//     });
+    
+//         // console.log(dataDemoVal)
+//      
+// // Call function to update the chart
+
+// // On change to the DOM, call getData()
+// d3.selectAll("#selDataset").on("change", getData)
 
 // Call function to update the chart
-
-// On change to the DOM, call getData()
-// d3.selectAll("#selDataset").on("change", getData);
-
+function optionChanged(selectName) {
+    Plotly.restyle("bar", "values", [selectName])
+    updateBargraph(selectName);
+  };
