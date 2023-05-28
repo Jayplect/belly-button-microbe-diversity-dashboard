878ff7f324588a16c6ff172cd1dee002a751a535
Jayplect <evansja.femi@gmail.com>
2023-05-28 06:41:41 -0400

    added dynamic gauge chart

--- static/js/app.js
+++ static/js/app.js
@@ -67,9 +67,9 @@ function updatePlots(selectName) {
             t: 100,
             b: 100
         },
-        font: {size: 15}
-        // height: 600,
-        // width: 800
+        font: {size: 15},
+        height: 500,
+        width: 700
     };
 
     // Render the plot to the div tag with id "bar"
@@ -105,11 +105,44 @@ function updatePlots(selectName) {
             b: 100
         },
         xaxis: {title: 'OTU ID'},
-        font: {size: 15}
+        font: {size: 15},
+        height: 500,
+        width: 1200
     };
     
     // Render the plot to the div tag with id "bubble"
     Plotly.newPlot("bubble", traceData2, layout2);
+
+//------------------------------Gauge Chart------------------------------//
+    // Add ploting paramters for Gauge chart
+    let sampleMeta = data.metadata.filter(select => select.id == selectName)[0]
+
+    let trace3 = {
+    domain: { x: [0, 1], y: [0, 1] },
+    value: sampleMeta.wfreq,
+    type: "indicator",
+    title: { text: "<em>Belly Button Washing Freq</em><br>Scrubs per week", font: { size: 24 } },
+    mode: "gauge+number",
+    steps: [
+        { range: [0, 1], color: "#f0f5b4" },
+        { range: [2, 3], color: "d4fbb4" },
+        { range: [3, 4], color: "#daf5b4" },
+        { range: [4, 5], color: "#caf5b4" },
+        { range: [5, 6], color: "#c4f5b4" },
+        { range: [6, 7], color: "#bdf5b4" },
+        { range: [7, 8], color: "#b1f5b4" },
+        { range: [8, 9], color: "#8ef5b4" }
+      ],
+    threshold: {
+        line: { color: "red", width: 4 },
+        thickness: 0.75,
+        value: 9}
+    };
+    // Data trace array
+    let traceData3 =[trace3];
+    
+    // Render the plot to the div tag with id "bubble"
+    Plotly.newPlot("gauge", traceData3)
 })};
 
 //------------------------------Demographic Info------------------------------//
@@ -118,7 +151,7 @@ function updateDemo(selectName) {
         // Fetch the JSON data
     d3.json(url).then(function(data){
 
-        // Add Demographic Info 
+    // Add Demographic Info 
     let sampleMeta = data.metadata.filter(select => select.id == selectName)[0]
     // set html to null
     d3.select(".panel-body").html("")
@@ -136,9 +169,6 @@ function optionChanged(selectName) {
     Plotly.restyle("bubble", "values", [selectName])
     updatePlots(selectName)
     updateDemo(selectName)
-    
-    // Plotly.restyle("bubble", "values", [selectName])
-    // updateChart(selectName)
   };
 
   init();
